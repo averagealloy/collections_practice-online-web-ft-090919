5b0536823c37b5a1c13ff777a787d4b721b5660b
Mike McTighe <m.mctighe10@gmail.com>
2019-09-13 18:37:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,7 +5,7 @@ def sort_array_desc(array)
 array.sort.reverse
 end
 def sort_array_char_count(array)
-array.sort {|left,right| left.length <=> right.lenght}
+array.sort {|left,right| left.length <=> right.length}
 end
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
