df6375d7433d4d4b4fcaba3c9a9be1dd9095fa85
Mike McTighe <m.mctighe10@gmail.com>
2019-09-13 18:29:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
-
+def sort_array_asc(array)
+array.sort
+end
+def sort_array_desc(array)
+array.sort.reverse
+end
+def sort_array_char_count(array)
+array.sort {|left,right| left.length <=> right.lenght}
+end
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+def reverse_array(array)
+array.reverse
+end
+def kesha_marker(array)
+new_array = []
+new_array << {array.each}
+end
