103fd69de95321cc87fb2a40825948c74363d957
Mike McTighe <m.mctighe10@gmail.com>
2019-09-13 19:17:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,3 +40,12 @@ end
   def sum_array(array)
     array.inject { |sum, element| sum + element}
   end
+  def add_s(array)
+     array.collect do |word|
+       if array[1] == word
+         word
+       else
+         word + "s"
+     end
+     end
+     end
